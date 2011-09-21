49ae6c2ebb132511c537629667d809706c4e235e
Brett Sutton <bsutton@noojeeit.com.au>
2011-09-21 23:23:04 +1000

    Added support for converting between the differnt amf supported units.

---
+++ convertor/src/main/java/threedc/github/com/model/UnitConversion.java
@@ -0,0 +1,74 @@
+package threedc.github.com.model;
+
+// Provides conversion factors between different units of measure.
+public class UnitConversion
+{
+
+	private final Units from;
+	private final Units to;
+	private final float conversionFactor;
+
+	public UnitConversion(Units from, Units to, float conversionFactor)
+	{
+		this.from = from;
+		this.to = to;
+		this.conversionFactor = conversionFactor;
+	}
+	
+	public float conversionFactor()
+	{
+		return this.conversionFactor;
+	}
+
+
+	static UnitConversion getUnitConversion(Units from, Units to)
+	{
+		UnitConversion result = null;
+
+		for (UnitConversion conversion : conversionTable)
+		{
+			if (conversion.from.equals(from) && conversion.to.equals(to))
+			{
+				result = conversion;
+				break;
+			}
+		}
+		
+		if (result == null)
+			throw new IllegalArgumentException("Unsupported conversion attempted: " + from.toString() + " to " + to.toString());
+
+		return result;
+	}
+
+	// Provides a list of standard conversion factors.
+	static UnitConversion[] conversionTable =
+		
+	{ new UnitConversion(Units.millimeter, Units.inch, 0.0393701F)
+	, new UnitConversion(Units.millimeter, Units.feet, 0.00328084F)
+	, new UnitConversion(Units.millimeter, Units.meter, 0.001F)
+	, new UnitConversion(Units.millimeter, Units.micron, 1000F)
+	
+	, new UnitConversion(Units.inch, Units.millimeter, 25.4F)
+	, new UnitConversion(Units.inch, Units.feet, 0.0833333F)
+	, new UnitConversion(Units.inch, Units.meter, 0.0254F)
+	, new UnitConversion(Units.inch, Units.micron, 25400F)
+	
+	, new UnitConversion(Units.feet, Units.millimeter, 304.8F)
+	, new UnitConversion(Units.feet, Units.inch, 12)
+	, new UnitConversion(Units.feet, Units.meter, 0.3048F)
+	, new UnitConversion(Units.feet, Units.micron, 304800F)
+	
+	, new UnitConversion(Units.meter, Units.millimeter, 1000F)
+	, new UnitConversion(Units.meter, Units.inch, 39.37008F)
+	, new UnitConversion(Units.meter, Units.feet, 3.28084F)
+	, new UnitConversion(Units.meter, Units.micron, 1000000F)
+	
+	, new UnitConversion(Units.micron, Units.millimeter, 0.001F)
+	, new UnitConversion(Units.micron, Units.inch, 0.00003937F)
+	, new UnitConversion(Units.micron, Units.feet, 0.000003281F)
+	, new UnitConversion(Units.micron, Units.meter, 0.000001F)
+
+	};
+
+
+}
--- convertor/src/main/java/threedc/github/com/model/Units.java
+++ convertor/src/main/java/threedc/github/com/model/Units.java
@@ -14,4 +14,32 @@ public enum Units
 	meter,
 	micron;
 
+	public static String valuesAsString()
+	{
+		StringBuffer sb = new StringBuffer();
+
+		for (Units unit : Units.values())
+		{
+			if (sb.length() > 0)
+				sb.append(", ");
+			
+			sb.append(unit.toString());
+		}
+		return sb.toString();
+	}
+	
+	/**
+	 * provides a conversion factor from this Unit to the 'To' units.
+	 * @param from
+	 * @param To
+	 * @return
+	 */
+	public float conversionFactor(Units to)
+	{
+		return UnitConversion.getUnitConversion(this, to).conversionFactor();
+	}
+	
+	
+
+
 }
