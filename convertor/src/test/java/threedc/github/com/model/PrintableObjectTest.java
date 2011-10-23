5e3a02598c1dcbc634340a64acac064963e868a7
bsutton <bsutton@noojee.com.au>
2011-10-23 21:30:15 +1100

    corrected some of the test cases.

--- convertor/src/test/java/threedc/github/com/model/PrintableObjectTest.java
+++ convertor/src/test/java/threedc/github/com/model/PrintableObjectTest.java
@@ -38,118 +38,134 @@ public class PrintableObjectTest
 	@Test
 	public void unitTransformTest()
 	{
-		PrintableObject po = new PrintableObject("1", VertexMode.Ordered, Units.feet);
-		int count = 10000000;
-		for (int i = 0; i < count; i++)
+		try
 		{
-			po.addVertex(new Vertex(1, 1, 1));
+			PrintableObject po = new PrintableObject("1", VertexMode.Ordered, Units.feet);
+			int count = 1000000;
+			for (int i = 0; i < count; i++)
+			{
+				po.addVertex(new Vertex(1, 1, 1));
+			}
+
+			Transform[] transforms = new Transform[]
+			{ new UnitTransform(Units.feet, Units.inch) };
+			po.applyTransforms(transforms);
+
+			for (Vertex vertex : po.getVertexes())
+			{
+				Assert.assertEquals(vertex.getX(), 12.0F);
+				Assert.assertEquals(vertex.getY(), 12.0F);
+				Assert.assertEquals(vertex.getZ(), 12.0F);
+			}
+			Assert.assertEquals(po.getVertexCount(), count);
+
+			po = new PrintableObject("1", VertexMode.Sorted, Units.feet);
+			for (int i = 0; i < count; i++)
+			{
+				po.addVertex(new Vertex(i, i, i));
+			}
+
+			transforms = new Transform[]
+			{ new UnitTransform(Units.feet, Units.inch) };
+			po.applyTransforms(transforms);
+
+			float conversion = UnitConversion.getUnitConversion(Units.feet, Units.inch).conversionFactor();
+			float i = 0;
+			for (Vertex vertex : po.getVertexes())
+			{
+				Assert.assertEquals(vertex.getX(), i * conversion);
+				Assert.assertEquals(vertex.getY(), i * conversion);
+				Assert.assertEquals(vertex.getZ(), i * conversion);
+				i++;
+			}
+			Assert.assertEquals(po.getVertexCount(), count);
 		}
-
-		Transform[] transforms = new Transform[]
-		{ new UnitTransform(Units.feet, Units.inch) };
-		po.applyTransforms(transforms);
-
-		for (Vertex vertex : po.getVertexes())
+		catch (Exception e)
 		{
-			Assert.assertEquals(vertex.getX(), 12.0F);
-			Assert.assertEquals(vertex.getY(), 12.0F);
-			Assert.assertEquals(vertex.getZ(), 12.0F);
+			logger.error(e, e);
+			Assert.fail(e.getMessage());
 		}
-		Assert.assertEquals(po.getVertexCount(), count);
-
-		po = new PrintableObject("1", VertexMode.Sorted, Units.feet);
-		for (int i = 0; i < count; i++)
-		{
-			po.addVertex(new Vertex(i, i, i));
-		}
-
-		transforms = new Transform[]
-		{ new UnitTransform(Units.feet, Units.inch) };
-		po.applyTransforms(transforms);
-
-		float conversion = UnitConversion.getUnitConversion(Units.feet, Units.inch).conversionFactor();
-		float i = 0;
-		for (Vertex vertex : po.getVertexes())
-		{
-			Assert.assertEquals(vertex.getX(), i * conversion);
-			Assert.assertEquals(vertex.getY(), i * conversion);
-			Assert.assertEquals(vertex.getZ(), i * conversion);
-			i++;
-		}
-		Assert.assertEquals(po.getVertexCount(), count);
 
 	}
 
 	@Test
 	public void rotationTransformTest()
 	{
-		PrintableObject po = new PrintableObject("1", VertexMode.Ordered, Units.millimeter);
-		int count = 10000;
-		po.addVertex(new Vertex(0, 0, 0));
-		for (int i = 0; i < count; i++)
+		try
 		{
+			PrintableObject po = new PrintableObject("1", VertexMode.Ordered, Units.millimeter);
+			int count = 10000;
+			po.addVertex(new Vertex(0, 0, 0));
+			for (int i = 0; i < count; i++)
+			{
+				po.addVertex(new Vertex(1, 1, 1));
+			}
+
+			Transform[] transforms = new Transform[]
+			{ new RotationTransform(90, 0, 0) };
+			po.applyTransforms(transforms);
+
+			int i = 0;
+			for (Vertex vertex : po.getVertexes())
+			{
+				if (i == 0)
+				{
+					Assert.assertEquals(0.0F, vertex.getX());
+					Assert.assertEquals(0.0F, vertex.getY());
+					Assert.assertEquals(0.0F, vertex.getZ());
+				}
+				else
+				{
+					Assert.assertEquals(1.0F, vertex.getX());
+					Assert.assertEquals(1.0F, vertex.getY());
+					Assert.assertEquals(-1.0F, vertex.getZ());
+				}
+				i++;
+			}
+			Assert.assertEquals(po.getVertexCount(), count + 1);
+
+			po = new PrintableObject("1", VertexMode.Sorted, Units.millimeter);
+			// We need to insert at least one different vertex otherwise we are
+			// dealing with a point
+			// and a point rotated on the spot looks just the same after it gets
+			// rotated.
 			po.addVertex(new Vertex(1, 1, 1));
+
+			for (i = 0; i < count; i++)
+			{
+				po.addVertex(new Vertex(2, 2, 2));
+			}
+
+			transforms = new Transform[]
+			{ new RotationTransform(180, 180, 180) };
+			po.applyTransforms(transforms);
+
+			i = 0;
+			for (Vertex vertex : po.getVertexes())
+			{
+				if (i == 0)
+				{
+					Assert.assertEquals(1.0F, vertex.getX());
+					Assert.assertEquals(1.0F, vertex.getY());
+					Assert.assertEquals(1.0F, vertex.getZ());
+				}
+				else
+				{
+
+					Assert.assertEquals(2.0F, vertex.getX());
+					Assert.assertEquals(2.0F, vertex.getY());
+					Assert.assertEquals(2.0F, vertex.getZ());
+				}
+				i++;
+			}
+			Assert.assertEquals(po.getVertexCount(), 2);
+
 		}
-
-		Transform[] transforms = new Transform[]
-		{ new RotationTransform(90, 0, 0) };
-		po.applyTransforms(transforms);
-
-		int i = 0;
-		for (Vertex vertex : po.getVertexes())
+		catch (Exception e)
 		{
-			if (i == 0)
-			{
-				Assert.assertEquals(0.0F, vertex.getX());
-				Assert.assertEquals(0.0F, vertex.getY());
-				Assert.assertEquals(0.0F, vertex.getZ());
-			}
-			else
-			{
-				Assert.assertEquals(1.0F, vertex.getX());
-				Assert.assertEquals(1.0F, vertex.getY());
-				Assert.assertEquals(0.0F, vertex.getZ());
-			}
-			i++;
+			logger.error(e, e);
+			Assert.fail(e.getMessage());
 		}
-		Assert.assertEquals(po.getVertexCount(), count);
-
-		po = new PrintableObject("1", VertexMode.Sorted, Units.millimeter);
-		// We need to insert at least one different vertex otherwise we are
-		// dealing with a point
-		// and a point rotated on the spot looks just the same after it gets
-		// rotated.
-		po.addVertex(new Vertex(1, 1, 1));
-
-		for (i = 0; i < count; i++)
-		{
-			po.addVertex(new Vertex(2, 2, 2));
-		}
-
-		transforms = new Transform[]
-		{ new RotationTransform(180, 180, 180) };
-		po.applyTransforms(transforms);
-
-		i = 0;
-		for (Vertex vertex : po.getVertexes())
-		{
-			if (i == 0)
-			{
-				Assert.assertEquals(vertex.getX(), 1);
-				Assert.assertEquals(vertex.getY(), 1);
-				Assert.assertEquals(vertex.getZ(), 1);
-			}
-			else
-			{
-
-				Assert.assertEquals(vertex.getX(), -1);
-				Assert.assertEquals(vertex.getY(), 0);
-				Assert.assertEquals(vertex.getZ(), -1);
-			}
-			i++;
-		}
-		Assert.assertEquals(po.getVertexCount(), count);
-
 	}
 
 }
